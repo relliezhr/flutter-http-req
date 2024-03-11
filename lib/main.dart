08a46b04c257b108b304bfa0bbf92d3b0f9b7d58
Aurellie Azzahra <aurellieazzahra07@gmail.com>
2024-03-11 21:44:56 +0700

    initial init

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:http_req/pages/movie_list.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Your Movies',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MovieList();
+  }
+}
---
+++ lib/models/movie.dart
@@ -0,0 +1,19 @@
+class Movie {
+  int id;
+  String title;
+  double voteAverage;
+  String overview;
+  String posterPath;
+
+  Movie(this.id, this.title, this.voteAverage, this.overview, this.posterPath);
+
+  factory Movie.fromJson(Map<String, dynamic> parsedJson) {
+    return Movie(
+      parsedJson['id'],
+      parsedJson['title'],
+      parsedJson['vote_average'] * 1.0,
+      parsedJson['overview'],
+      parsedJson['poster_path'],
+    );
+  }
+}
